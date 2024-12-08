663309ca470362e09757aa14354d5f0e04d7b136
Dev-FStacker <bennguyen.contact@gmail.com>
2024-12-08 17:19:00 +0700

    fix-navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,46 +1,59 @@
 import "../styles/Header.css";
-import HamburgerIcon from "../assets/hamburger.png";
+import Logo from "../assets/logo-website.png";
 import SearchIcon from "../assets/search.png";
-import UserIcon from "../assets/user-icon.png";
-import ShoppingCart from "../assets/shopping-bag.png";
+import UserIcon from "../assets/user.png";
+import ShoppingCart from "../assets/shopping-cart.png";
 const Header = () => {
   return (
-    <div>
-      <div class="header">
-        <div class="left-section">
-          <button class="hamburger-menu">
-            <img class="hamburger-icon" src={HamburgerIcon} alt="" />
-          </button>
-        </div>
-        <div class="middle-section">
-          <form class="form-search" action="#">
-            <input
-              class="search-bar"
-              type="text"
-              name="keyword"
-              placeholder="Tìm sản phẩm, thương hiệu, và tên shop"
-            />
-            <button class="search-button">
-              <img class="search-icon" src={SearchIcon} alt="" />
-            </button>
-          </form>
-        </div>
-        <div class="right-section">
-          <div class="right-grid">
-            <div class="cart">
-              <button class="cart-button">
-                <img class="cart-icon" src={ShoppingCart} alt="" />
+    <nav class="navbar d-flex flex-wrap fixed-top navbar-expand-sm bg-dark navbar-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="/">
+          <div class="logo-toggle">
+            <img src={Logo} alt="" class="logo" />
+            <span class="brand-text">SkyLines</span>
+          </div>
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#collapsibleNavbar"
+          aria-controls="collapsibleNavbar"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+          <ul class="navbar-nav mx-auto">
+            <li class="nav-item">
+              <form class="d-flex">
+                <input
+                  class="search-bar"
+                  type="text"
+                  name="keyword"
+                  placeholder="Search..."
+                  aria-label="Search"
+                />
+                <button class="search-button bg-light" type="submit">
+                  <img class="search-icon" src={SearchIcon} alt="" />
+                </button>
+              </form>
+            </li>
+            <li class="nav-item">
+              <button class="cart-button" href="/">
+                <img src={ShoppingCart} class="cart-icon" alt="" />
               </button>
-            </div>
-            <div class="profile-picture">
-              <button class="profile-button">
+            </li>
+            <li class="nav-item">
+              <button class="user-button">
                 <img class="user-icon" src={UserIcon} alt="" />
               </button>
-            </div>
-          </div>
+            </li>
+          </ul>
         </div>
       </div>
-    </div>
+    </nav>
   );
 };
 
